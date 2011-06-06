93867645d21bb95221e0aa86b8dc15eaf6b8e23a
Tom Finegan <tomfinegan@google.com>
2011-06-06 18:25:30 -0400

    http_client: rename http_client_main.h to http_client_base.h
    - I need to include Windows.h first in most places anyway, and the new name
      (hopefully) helps explain why we include it first everywhere.
    
    Change-Id: I4090fee58642d1fd370c06b91a71be4f208cfcdd

---
+++ http_client/http_client_base.h
@@ -0,0 +1,17 @@
+// Copyright (c) 2011 The WebM project authors. All Rights Reserved.
+//
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file in the root of the source
+// tree. An additional intellectual property rights grant can be found
+// in the file PATENTS.  All contributing project authors may
+// be found in the AUTHORS file in the root of the source tree.
+#ifndef WEBMLIVE_HTTP_CLIENT_BASE_H
+#define WEBMLIVE_HTTP_CLIENT_BASE_H
+#pragma once
+
+#ifndef _WIN32_WINNT
+#define _WIN32_WINNT 0x0501 // WinXP
+#endif
+
+#include "Windows.h"
+#endif // WEBMLIVE_HTTP_CLIENT_BASE_H
