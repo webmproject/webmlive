49390bbe3f048603ee30c36421f03d3594a041e1
Tom Finegan <tomfinegan@google.com>
2011-06-13 23:13:41 -0400

    http_client_main: add missing copyright header
    Change-Id: I3062f1be8b8f88cc69c402b044622cb5cd6a6f74

--- http_client/http_client_main.cc
+++ http_client/http_client_main.cc
@@ -1,6 +1,10 @@
-// webmtestshell_2008.cpp : Defines the entry point for the console application.
+// Copyright (c) 2011 The WebM project authors. All Rights Reserved.
 //
-
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file in the root of the source
+// tree. An additional intellectual property rights grant can be found
+// in the file PATENTS.  All contributing project authors may
+// be found in the AUTHORS file in the root of the source tree.
 #include "http_client_base.h"
 
 #include <conio.h>
