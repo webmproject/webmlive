e0fe0870fad5f53c59aab038987d0be552c50c15
Tom Finegan <tomfinegan@google.com>
2011-06-30 03:32:54 -0400

    file_util: add stat based implementation of get_file_size
    - Keep GetFileAttributesEx version around.  For now, at least.
    
    Change-Id: Ia84985db66d7113a457f7d562495ab2b8c22fa4b

--- http_client/file_util_win.cc
+++ http_client/file_util_win.cc
@@ -9,6 +9,9 @@
 #include "http_client_base.h"
 #include "file_util.h"
 
+#include <sys/types.h>
+#include <sys/stat.h>
+
 #include <shlwapi.h>
 
 bool WebmLive::FileUtil::file_exists(std::wstring path)
@@ -19,9 +22,17 @@ bool WebmLive::FileUtil::file_exists(std::wstring path)
 uint64 WebmLive::FileUtil::get_file_size(std::wstring path)
 {
   uint64 file_size = 0;
+#if 0
   WIN32_FILE_ATTRIBUTE_DATA attr = {0};
   if (GetFileAttributesEx(path.c_str(), GetFileExInfoStandard, &attr)) {
     file_size = (attr.nFileSizeHigh << 4) + attr.nFileSizeLow;
   }
+#else
+  struct _stat statbuf = {0};
+  int result = _wstat(path.c_str(), &statbuf);
+  if (result == NO_ERROR) {
+    file_size = statbuf.st_size;
+  }
+#endif
   return file_size;
 }
