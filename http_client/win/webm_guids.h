0f15822758a35bed7f7d56d14ebd219adb3e9cc6
Tom Finegan <tomfinegan@google.com>
2011-09-22 18:48:36 -0400

    Add video AM_MEDIA_TYPE management classes.
    Add MediaType and VideoMediaType for dealing with
    AM_MEDIA_TYPE structures containing VIDEOINFOHEADER
    and VIDEOINFOHEADER2 format blobs. Support
    configuration of the header using
    WebmEncoderConfig::VideoCaptureConfig structs. For
    use w/video input device configuration and logging.
    
    Change-Id: Ia7af4718d9bee5a01a53168d3deb4753924211fd

---
+++ http_client/win/webm_guids.h
@@ -0,0 +1,18 @@
+// Copyright (c) 2011 The WebM project authors. All Rights Reserved.
+//
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file in the root of the source
+// tree. An additional intellectual property rights grant can be found
+// in the file PATENTS.  All contributing project authors may
+// be found in the AUTHORS file in the root of the source tree.
+
+#ifndef HTTP_CLIENT_WIN_WEBM_GUIDS_H_
+#define HTTP_CLIENT_WIN_WEBM_GUIDS_H_
+
+#include <objbase.h>
+
+namespace webmlive {
+  extern const GUID MEDIASUBTYPE_I420;
+}  // namespace webmlive
+
+#endif  // HTTP_CLIENT_WIN_WEBM_GUIDS_H_
