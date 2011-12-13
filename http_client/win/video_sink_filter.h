be7e4edac5fb2137b584254b17dc1baf3a7c796c
Tom Finegan <tomfinegan@google.com>
2011-12-13 17:38:53 -0500

    Add video sink filter include.
    Contains only include guards and copyright block.
    
    Change-Id: I09f9910a83c0362ad8450a8f219b4190f8ce5d0a

---
+++ http_client/win/video_sink_filter.h
@@ -0,0 +1,11 @@
+// Copyright (c) 2011 The WebM project authors. All Rights Reserved.
+//
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file in the root of the source
+// tree. An additional intellectual property rights grant can be found
+// in the file PATENTS.  All contributing project authors may
+// be found in the AUTHORS file in the root of the source tree.
+#ifndef HTTP_CLIENT_WIN_VIDEO_SINK_FILTER_H_
+#define HTTP_CLIENT_WIN_VIDEO_SINK_FILTER_H_
+
+#endif  // HTTP_CLIENT_WIN_VIDEO_SINK_FILTER_H_
