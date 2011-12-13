1711c75bca30936205302a7b4e3aa3a86482ab16
Tom Finegan <tomfinegan@google.com>
2011-12-13 18:05:36 -0500

    Add video encoder sources.
    - Add to vcproj
    - Contain only copyright blocks/include guards.
    
    Change-Id: I93b3e4386cfeb04ae31b216ce2dc475b1462cca8

---
+++ http_client/video_encoder.h
@@ -0,0 +1,12 @@
+// Copyright (c) 2011 The WebM project authors. All Rights Reserved.
+//
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file in the root of the source
+// tree. An additional intellectual property rights grant can be found
+// in the file PATENTS.  All contributing project authors may
+// be found in the AUTHORS file in the root of the source tree.
+
+#ifndef HTTP_CLIENT_VIDEO_ENCODER_H_
+#define HTTP_CLIENT_VIDEO_ENCODER_H_
+
+#endif  // HTTP_CLIENT_VIDEO_ENCODER_H_
