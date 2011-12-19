5da4abfe36d0b9b2fea9806891ec354416b65ebd
Tom Finegan <tomfinegan@google.com>
2011-12-19 15:25:16 -0500

    video_encoder: add VideoFrame class.
    Supports storage of I420 and VP8 frames.
    
    Change-Id: I40c59db6077d9924039e9ee4b5581e048c47d72e

--- http_client/video_encoder.cc
+++ http_client/video_encoder.cc
@@ -6,3 +6,126 @@
 // in the file PATENTS.  All contributing project authors may
 // be found in the AUTHORS file in the root of the source tree.
 
+#include "http_client/video_encoder.h"
+
+#include <new>
+
+#include "glog/logging.h"
+
+namespace webmlive {
+
+VideoFrame::VideoFrame()
+    : keyframe_(false),
+      width_(0),
+      height_(0),
+      timestamp_(0),
+      duration_(0),
+      buffer_capacity_(0),
+      buffer_length_(0),
+      format_(kVideoFormatI420) {
+}
+
+VideoFrame::~VideoFrame() {
+}
+
+int32 VideoFrame::Init(VideoFormat format,
+                       bool keyframe,
+                       int32 width,
+                       int32 height,
+                       int64 timestamp,
+                       int64 duration,
+                       const uint8* ptr_data,
+                       int32 data_length) {
+  if (!ptr_data) {
+    LOG(ERROR) << "VideoFrame can't Init with NULL data pointer.";
+    return kInvalidArg;
+  }
+  if (format != kVideoFormatI420 && format != kVideoFormatVP8) {
+    LOG(ERROR) << "Unknown VideoFormat.";
+    return kInvalidArg;
+  }
+  if (data_length > buffer_capacity_) {
+    buffer_.reset(new (std::nothrow) uint8[data_length]);  // NOLINT(nothrow)
+    if (!buffer_) {
+      LOG(ERROR) << "VideoFrame Init cannot allocate buffer.";
+      return kNoMemory;
+    }
+    buffer_capacity_ = data_length;
+  }
+  memcpy(&buffer_[0], ptr_data, data_length);
+  buffer_length_ = data_length;
+  format_ = format;
+  keyframe_ = keyframe;
+  width_ = width;
+  height_ = height;
+  timestamp_ = timestamp;
+  duration_ = duration;
+  return kSuccess;
+}
+
+int32 VideoFrame::Clone(VideoFrame* ptr_frame) const {
+  if (!ptr_frame) {
+    LOG(ERROR) << "cannot Clone to a NULL VideoFrame.";
+    return kInvalidArg;
+  }
+  if (buffer_.get() && buffer_capacity_ > 0) {
+    ptr_frame->buffer_.reset(
+        new (std::nothrow) uint8[buffer_capacity_]);  // NOLINT(nothrow)
+    if (!ptr_frame->buffer_) {
+      LOG(ERROR) << "VideoFrame Clone cannot allocate buffer.";
+      return kNoMemory;
+    }
+    memcpy(&ptr_frame->buffer_[0], &buffer_[0], buffer_length_);
+  }
+  ptr_frame->buffer_capacity_ = buffer_capacity_;
+  ptr_frame->buffer_length_ = buffer_length_;
+  ptr_frame->format_ = format_;
+  ptr_frame->keyframe_ = keyframe_;
+  ptr_frame->width_ = width_;
+  ptr_frame->height_ = height_;
+  ptr_frame->timestamp_ = timestamp_;
+  ptr_frame->duration_ = duration_;
+  return kSuccess;
+}
+
+void VideoFrame::Swap(VideoFrame* ptr_frame) {
+  CHECK_NOTNULL(buffer_.get());
+  CHECK_NOTNULL(ptr_frame->buffer_.get());
+
+  const VideoFormat temp_format = format_;
+  format_ = ptr_frame->format_;
+  ptr_frame->format_ = temp_format;
+
+  const bool temp_keyframe = keyframe_;
+  keyframe_ = ptr_frame->keyframe_;
+  ptr_frame->keyframe_ = temp_keyframe;
+
+  int32 temp = width_;
+  width_ = ptr_frame->width_;
+  ptr_frame->width_ = temp;
+
+  temp = height_;
+  height_ = ptr_frame->height_;
+  ptr_frame->height_ = temp;
+
+  int64 temp_time = timestamp_;
+  timestamp_ = ptr_frame->timestamp_;
+  ptr_frame->timestamp_ = temp_time;
+
+  temp_time = duration_;
+  duration_ = ptr_frame->duration_;
+  ptr_frame->duration_ = temp_time;
+
+  buffer_.swap(ptr_frame->buffer_);
+
+  temp = buffer_capacity_;
+  buffer_capacity_ = ptr_frame->buffer_capacity_;
+  ptr_frame->buffer_capacity_ = temp;
+
+  temp = buffer_length_;
+  buffer_length_ = ptr_frame->buffer_length_;
+  ptr_frame->buffer_length_ = temp;
+}
+
+}  // namespace webmlive
+
--- http_client/video_encoder.h
+++ http_client/video_encoder.h
@@ -9,11 +9,68 @@
 #ifndef HTTP_CLIENT_VIDEO_ENCODER_H_
 #define HTTP_CLIENT_VIDEO_ENCODER_H_
 
+#include "boost/scoped_array.hpp"
+#include "http_client/basictypes.h"
+#include "http_client/http_client_base.h"
+
 namespace webmlive {
 
-// Temporary forward declaration of |VideoFrame| for
-// |VideoFrameCallbackInterface|.
-class VideoFrame;
+enum VideoFormat {
+  kVideoFormatI420 = 0,
+  kVideoFormatVP8 = 1,
+  kVideoFormatCount = 2,
+};
+
+// Storage class for video frames. Supports I420 and VP8 frames.
+class VideoFrame {
+ public:
+  enum {
+    kNoMemory = -2,
+    kInvalidArg = -1,
+    kSuccess = 0,
+  };
+  VideoFrame();
+  ~VideoFrame();
+
+  // Allocates storage for |ptr_data|, sets internal fields to values of
+  // caller's args, and returns |kSuccess|. Returns |kInvalidArg| when
+  // |ptr_data| is NULL, and |format| is not |kVideoFormatI420| or
+  // |kVideoFormatVP8|. Returns |kNoMemory| when unable to allocate storage
+  // for |ptr_data|.
+  int32 Init(VideoFormat format, bool keyframe, int32 width, int32 height,
+             int64 timestamp, int64 duration, const uint8* ptr_data,
+             int32 data_length);
+
+  // Copies |VideoFrame| data to |ptr_frame|. Performs allocation if necessary.
+  // Returns |kSuccess| when successful. Returns |kInvalidArg| when |ptr_frame|
+  // is NULL. Returns |kNoMemory| when memory allocation fails.
+  int32 Clone(VideoFrame* ptr_frame) const;
+
+  // Swaps |VideoFrame| member data with |ptr_frame|'s. The |VideoFrame|s
+  // must have non-NULL buffers.
+  void Swap(VideoFrame* ptr_frame);
+  bool keyframe() const { return keyframe_; }
+  int32 width() const { return width_; }
+  int32 height() const { return height_; }
+  int64 timestamp() const { return timestamp_; }
+  int64 duration() const { return duration_; }
+  uint8* buffer() const { return buffer_.get(); }
+  int32 buffer_length() const { return buffer_length_; }
+  int32 buffer_capacity() const { return buffer_capacity_; }
+  VideoFormat format() const { return format_; }
+
+ private:
+  bool keyframe_;
+  int32 width_;
+  int32 height_;
+  int64 timestamp_;
+  int64 duration_;
+  boost::scoped_array<uint8> buffer_;
+  int32 buffer_capacity_;
+  int32 buffer_length_;
+  VideoFormat format_;
+  WEBMLIVE_DISALLOW_COPY_AND_ASSIGN(VideoFrame);
+};
 
 // Pure interface class that provides a simple callback allowing the
 // implementor class to receive |VideoFrame| pointers.
@@ -57,3 +114,4 @@ struct VpxConfig {
 }  // namespace webmlive
 
 #endif  // HTTP_CLIENT_VIDEO_ENCODER_H_
+
