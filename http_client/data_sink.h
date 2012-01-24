bb18ef14ae78cef8d4b7ddc406ca60dc14f8b0eb
Tom Finegan <tomfinegan@google.com>
2012-01-24 10:14:02 -0500

    Add DataSinkInterface.
    Add simple interface class for writing data to
    sink-like objects.
    
    Change-Id: Id09379f25b35eae376977c6f0f967656b0bba53f

---
+++ http_client/data_sink.h
@@ -0,0 +1,29 @@
+// Copyright (c) 2012 The WebM project authors. All Rights Reserved.
+//
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file in the root of the source
+// tree. An additional intellectual property rights grant can be found
+// in the file PATENTS.  All contributing project authors may
+// be found in the AUTHORS file in the root of the source tree.
+#ifndef HTTP_CLIENT_DATA_SINK_H_
+#define HTTP_CLIENT_DATA_SINK_H_
+
+#include "http_client/basictypes.h"
+
+namespace webmlive {
+
+class DataSinkInterface {
+ public:
+  virtual ~DataSinkInterface() {};
+
+  // Returns true when the class implementing |DataSinkInterface| is ready to
+  // receive data via a call to |WriteData()|.
+  virtual bool Ready() const = 0;
+
+  // Writes data to the sink and returns true when successful.
+  virtual bool WriteData(const uint8* ptr_data, int32 data_length) = 0;
+};
+
+}  // namespace webmlive
+
+#endif  // HTTP_CLIENT_DATA_SINK_H_
