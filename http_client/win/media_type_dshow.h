8fecfb4797f68bd0f55257deea25152fb001be39
Tom Finegan <tomfinegan@google.com>
2012-01-30 14:41:12 -0500

    media_type_dshow: Cosmetics.
    Change-Id: Ifb8dcea84b00da1c68a1e31ae2a708986707476a

--- http_client/win/media_type_dshow.h
+++ http_client/win/media_type_dshow.h
@@ -33,19 +33,25 @@ class MediaType {
   };
   MediaType();
   virtual ~MediaType();
+
   // Returns |ptr_type_|.
   const AM_MEDIA_TYPE* get() const { return ptr_type_; }
+
   virtual int Init(const GUID& major_type, const GUID& format_type) = 0;
   virtual int Init(const AM_MEDIA_TYPE& media_type) = 0;
   virtual int Init() = 0;
+
   // Utility functions for free'ing |AM_MEDIA_TYPE| pointers.
   static void FreeMediaType(AM_MEDIA_TYPE* ptr_media_type);
   static void FreeMediaTypeData(AM_MEDIA_TYPE* ptr_media_type);
+
  private:
   // Allocates memory for |ptr_type_|.
   int AllocTypeStruct();
+
   // Allocates memory for |ptr_type_->pbFormat|.
   int AllocFormatBlob(SIZE_T blob_size);
+
   // Pointer to AM_MEDIA_TYPE memory allocated via CoTaskMemAlloc.
   AM_MEDIA_TYPE* ptr_type_;
   friend class AudioMediaType;
@@ -71,43 +77,54 @@ class VideoMediaType : public MediaType {
   static const int kYUYVBitCount = kYUY2BitCount;
   VideoMediaType();
   virtual ~VideoMediaType();
+
   // Allocates AM_MEDIA_TYPE for specified |major_type| and |format_type|.
   // Supports only |major_type| equal to MEDIATYPE_Video, and |format_type|'s
   // FORMAT_VideoInfo and FORMAT_VideoInfo2. Returns |kSuccess| for supported
   // types.
   virtual int Init(const GUID& major_type, const GUID& format_type);
+
   // Copies |media_type| and returns |kSuccess|.
   virtual int Init(const AM_MEDIA_TYPE& media_type);
+
   // Allocates AM_MEDIA_TYPE with VIDEOINFOHEADER format blob, and sets
   // contents to 0.
   virtual int Init();
+
   // Configures format block using |sub_type| and |config|. Directly applies
   // settings specified by |config| and returns success for supported
   // |sub_type| values. Note that not all |VideoSubType| values are supported.
   int ConfigureSubType(VideoSubType sub_type, const VideoConfig& config);
+
   // Mutators that allow direct control of frame rate without touching the
   // BITMAPINFOHEADER.
   int set_avg_time_per_frame(REFERENCE_TIME time_per_frame);
   int set_frame_rate(double frame_rate);
+
   // Accessors for frame rate, time per frame, width, and height.
   double frame_rate() const;
   REFERENCE_TIME avg_time_per_frame() const;
   int width() const;
   int height() const;
+
  private:
   // Easy access helper for obtaining values from the BITMAPINFOHEADER within
   // |ptr_type_|'s format blob.
   const BITMAPINFOHEADER* bitmap_header() const;
+
   // Configures |header| for specified |subtype| using values from |config|.
   int ConfigureFormatInfo(const VideoConfig& config, VideoSubType sub_type,
                           BITMAPINFOHEADER& header);
+
   // Sets source and target rectangles to respect |config.width| and
   // |config.height|.
   void ConfigureRects(const VideoConfig& config, RECT& source, RECT& target);
+
   // Sets VIDEOINFOHEADER members within pbFormat block of AM_MEDIA_TYPE.
   int ConfigureVideoInfoHeader(const VideoConfig& config,
                                VideoSubType sub_type,
                                VIDEOINFOHEADER* header);
+
   // Sets VIDEOINFOHEADER2 members within pbFormat block of AM_MEDIA_TYPE.
   int ConfigureVideoInfoHeader2(const VideoConfig& config,
                                 VideoSubType sub_type,
@@ -122,13 +139,17 @@ class AudioMediaType : public MediaType {
   typedef WebmEncoderConfig::AudioCaptureConfig AudioConfig;
   AudioMediaType();
   virtual ~AudioMediaType();
+
   // Allocates AM_MEDIA_TYPE for specified |major_type| and |format_type|.
   virtual int Init(const GUID& major_type, const GUID& format_type);
+
   // Copies |media_type| and returns |kSuccess|.
   virtual int Init(const AM_MEDIA_TYPE& media_type);
+
   // Allocates AM_MEDIA_TYPE with WAVEFORMATEX format blob, and sets contents
   // to 0.
   virtual int Init();
+
   // Validates |ptr_type_| and verifies that format blob has capacity for a
   // WAVEFORMATEX struct.
   bool IsValidWaveFormatExBlob() const;
@@ -150,19 +171,25 @@ class MediaTypePtr {
   MediaTypePtr();
   explicit MediaTypePtr(AM_MEDIA_TYPE* ptr_type);
   ~MediaTypePtr();
+
   // |Free()|s |ptr_type_| and takes ownership of |ptr_type|. Note that
   // |ptr_type| and the format blob within must be memory allocated via
   // CoTaskMemAlloc. Both pointers will be be passed to CoTaskMemFree in
   // |~MediaTypePtr()|.
   int Attach(AM_MEDIA_TYPE* ptr_type);
+
   // |Free()|s |ptr_type_| and sets it to NULL.
   void Free();
+
   // Returns |ptr_type_| and sets internal copy to NULL.
   AM_MEDIA_TYPE* Detach();
+
   // Returns |ptr_type_|.
   AM_MEDIA_TYPE* get() const { return ptr_type_; }
+
   // |Free()|s and returns pointer to |ptr_type_|.
   AM_MEDIA_TYPE** GetPtr();
+
  private:
   AM_MEDIA_TYPE* ptr_type_;
   WEBMLIVE_DISALLOW_COPY_AND_ASSIGN(MediaTypePtr);
