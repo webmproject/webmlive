acb65bc4f49af449385414a1181d25056413dc88
Tom Finegan <tomfinegan@google.com>
2012-03-21 17:21:06 -0400

    Add dshow_util.
    Move some messy DirectShow stuff into a single source file.
    
    Change-Id: I684a6850b797e12d062d87b2b712bd4457bcd275

---
+++ client_encoder/win/dshow_util.h
@@ -0,0 +1,21 @@
+// Copyright (c) 2012 The WebM project authors. All Rights Reserved.
+//
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file in the root of the source
+// tree. An additional intellectual property rights grant can be found
+// in the file PATENTS.  All contributing project authors may
+// be found in the AUTHORS file in the root of the source tree.
+// Extracts error from the HRESULT, and outputs its hex and decimal values.
+#ifndef CLIENT_ENCODER_WIN_DSHOW_UTIL_H_
+#define CLIENT_ENCODER_WIN_DSHOW_UTIL_H_
+
+#include <ios>
+
+#include "webmdshow/common/hrtext.hpp"
+#include "webmdshow/common/odbgstream.hpp"
+
+#define HRLOG(X) \
+    " {" << #X << "=" << X << "/" << std::hex << X << std::dec << " (" << \
+    hrtext(X) << ")}"
+
+#endif  // CLIENT_ENCODER_WIN_DSHOW_UTIL_H_
