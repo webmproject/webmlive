226eda98512602b258b367f396abf1f83a9a5450
Tom Finegan <tomfinegan@google.com>
2012-04-20 16:49:25 -0400

    Add audio encoding sources.
    Add audio/vorbis_encoder source files to vcproj.
    Files include only copyright blobs.
    
    Change-Id: I569d250a8b09e85d2d004cf124c8e6bf9f270c39

---
+++ client_encoder/audio_encoder.h
@@ -0,0 +1,11 @@
+// Copyright (c) 2012 The WebM project authors. All Rights Reserved.
+//
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file in the root of the source
+// tree. An additional intellectual property rights grant can be found
+// in the file PATENTS.  All contributing project authors may
+// be found in the AUTHORS file in the root of the source tree.
+#ifndef CLIENT_ENCODER_AUDIO_ENCODER_H_
+#define CLIENT_ENCODER_AUDIO_ENCODER_H_
+
+#endif  // CLIENT_ENCODER_AUDIO_ENCODER_H_
