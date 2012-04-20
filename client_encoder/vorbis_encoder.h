5f7767bff3811cea8a1adefa25c7cf1796d3608f
Tom Finegan <tomfinegan@google.com>
2012-04-20 17:34:07 -0400

    WIP: vorbis audio encoding.
    Change-Id: Ia1d115cdb77853cd429c4795616fc923bd66555b

--- client_encoder/vorbis_encoder.h
+++ client_encoder/vorbis_encoder.h
@@ -8,4 +8,38 @@
 #ifndef CLIENT_ENCODER_VORBIS_ENCODER_H_
 #define CLIENT_ENCODER_VORBIS_ENCODER_H_
 
+#include "client_encoder/audio_encoder.h"
+#include "client_encoder/webm_encoder.h"
+#include "libvorbis/vorbis/codec.h"
+#include "libvorbis/vorbis/vorbisenc.h"
+
+namespace webmlive {
+
+class VorbisEncoder {
+ public:
+  enum {
+    kCodecError = -202,
+    kEncoderError = -201,
+    kUnsupportedFormat = -200,
+    kInvalidArg = -1,
+    kSuccess = 0,
+    kNeedMoreSamples = 1,
+  };
+
+  VorbisEncoder();
+  ~VorbisEncoder();
+
+  int Init(const WebmEncoderConfig& config);
+  int EncodeBuffer(const AudioBuffer& uncompressed_buffer,
+                   AudioBuffer* ptr_vorbis_buffer);
+ private:
+  template <typename T> int CodecControl(int control_id, T val);
+  vorbis_info info_;
+  vorbis_dsp_state dsp_state_;
+  vorbis_block block_;
+  WEBMLIVE_DISALLOW_COPY_AND_ASSIGN(VorbisEncoder);
+};
+
+}  // namespace webmlive
+
 #endif  // CLIENT_ENCODER_VORBIS_ENCODER_H_
