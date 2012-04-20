8f6101a39f868c01cb0568e578c052648fd5a7a3
Tom Finegan <tomfinegan@google.com>
2012-04-20 16:54:54 -0400

    Move AudioConfig out of WebmEncoder.
    Move AudioConfig definition to audio_encoder.h, and
    remove the WebmEncoderConfig::AudioConfig typedefs.
    
    Change-Id: I687ee31f6f9950bd085446c0f6a43797a59b0d2a

--- client_encoder/audio_encoder.h
+++ client_encoder/audio_encoder.h
@@ -8,4 +8,40 @@
 #ifndef CLIENT_ENCODER_AUDIO_ENCODER_H_
 #define CLIENT_ENCODER_AUDIO_ENCODER_H_
 
+#include "boost/scoped_array.hpp"
+#include "boost/scoped_ptr.hpp"
+#include "client_encoder/basictypes.h"
+#include "client_encoder/client_encoder_base.h"
+
+namespace webmlive {
+
+enum AudioFormat {
+  kAudioFormatPcm = 1,
+  kAudioFormatVorbis = 2,
+  kAudioFormatIeeeFloat = 3,
+};
+
+// Audio configuration control structure. Values set to 0 mean use default.
+// Only |channels|, |sample_rate|, and |bits_per_sample| are user
+// configurable.
+struct AudioConfig {
+  AudioConfig()
+      : format_tag(kAudioFormatPcm),
+        channels(2),
+        bytes_per_second(0),
+        sample_rate(44100),
+        block_align(0),
+        bits_per_sample(16),
+        valid_bits_per_sample(0),
+        channel_mask(0) {}
+
+  uint16 format_tag;              // Audio format.
+  uint16 channels;                // Number of channels.
+  uint32 sample_rate;             // Samples per second.
+  uint32 bytes_per_second;        // Average bytes per second.
+  uint16 block_align;             // Atomic audio unit size in bytes.
+  uint16 bits_per_sample;         // Sample container size.
+  uint16 valid_bits_per_sample;   // Valid bits in sample container.
+  uint32 channel_mask;            // Channels present in audio stream.
+};
 #endif  // CLIENT_ENCODER_AUDIO_ENCODER_H_
