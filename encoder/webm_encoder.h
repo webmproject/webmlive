ccddccf3cf62511529b2361eedd726a98d4c63dc
Tom Finegan <tomfinegan@google.com>
2015-03-17 18:01:25 -0700

    encoder: Correctly handle running out of video buffers.
    WIP/not finished
    
    Change-Id: Ie9c72bf52c6d243b24a6cbca25361e9559e43ec7

--- encoder/webm_encoder.h
+++ encoder/webm_encoder.h
@@ -210,6 +210,9 @@ class WebmEncoder : public AudioSamplesCallbackInterface,
   // timestamp.
   int WaitForSamples();
 
+  // Returns timestamp of next available video frame.
+  int PeekVideoTimestamp(int64* timestamp);
+
   // Set to true when |Init()| is successful.
   bool initialized_;
 
