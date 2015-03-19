22c2095dd14ddcab7ef9664a7039098c547dd273
Tom Finegan <tomfinegan@google.com>
2015-03-18 20:09:01 -0700

    WIP: Add DASH encoding support.
    dash_writer:
    - Add chunk identifier utility function.
    - Simplify usage; get rid of the DashConfig* arg to Init().
    - Ditto for WriteManifest().
    
    webm_encoder:
    - Add audio and video muxers to WebmEncoder.
    - Refactor all WebmEncoder call sites that interact with
      LiveWebmMuxer to supprot dealing with multiple muxers.
    - Add DASH specific encoder loop function.
    
    webm_mux:
    - Add facilities for tracking muxer and chunk number to make
      use of multiple muxers easier.
    
    Change-Id: I3859f9d6b068d906e3f88dfea0493f0d45636d4f

--- encoder/dash_writer.h
+++ encoder/dash_writer.h
@@ -109,13 +109,15 @@ class DashWriter {
   // them in |config|. Must be called before |WriteManifest()|. Returns true
   // when successful.
   bool Init(std::string name, std::string id,
-            const WebmEncoderConfig& webm_config, DashConfig* config);
+            const WebmEncoderConfig& webm_config);
 
   // Writes the DASH manifest built from |config| to |manifest|. Returns true
   // when successful.
-  bool WriteManifest(const DashConfig& config,
-                     std::string* manifest);
+  bool WriteManifest(std::string* manifest);
 
+  // Returns a string suitable for identifying a chunk.
+  void IdForChunk(AdaptationSet::MediaType media_type, int64 chunk_num,
+                  std::string* chunk_id) const;
  private:
   void WriteAudioAdaptationSet(std::string* adaptation_set);
   void WriteVideoAdaptationSet(std::string* adaptation_set);
@@ -127,6 +129,8 @@ class DashWriter {
   bool initialized_;
   DashConfig config_;
   std::string indent_;
+  std::string name_;
+  std::string id_;
 };
 
 }  // namespace webmlive
