19f2c163d7433c9d58a6f0a37e9369815a83bc44
Tom Finegan <tomfinegan@google.com>
2015-04-22 14:29:06 -0700

    Add time_util.
    Time string utility functions.
    
    Change-Id: I0ffdc54f386a58f697e18601eaeadebaf041c3f7

---
+++ encoder/time_util.h
@@ -0,0 +1,29 @@
+// Copyright (c) 2015 The WebM project authors. All Rights Reserved.
+//
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file in the root of the source
+// tree. An additional intellectual property rights grant can be found
+// in the file PATENTS.  All contributing project authors may
+// be found in the AUTHORS file in the root of the source tree.
+#ifndef WEBMLIVE_ENCODER_TIME_UTIL_H_
+#define WEBMLIVE_ENCODER_TIME_UTIL_H_
+
+#include <string>
+
+namespace webmlive {
+
+// Time string utility functions. All return the string noted or an empty string
+// upon failure.
+
+// Returns current date in the formet: YYYYMMDD.
+std::string DateString();
+
+// Returns current time in the format: HHMMSS.
+std::string TimeString();
+
+// Returns user requested time string.
+std::string StrFTime(const std::string& format_string);
+
+}  // namespace webmlive
+
+#endif  // WEBMLIVE_ENCODER_TIME_UTIL_H_
